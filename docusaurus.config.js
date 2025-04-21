0efd101633352242aad74d348aec08856b249fb6
Nithin Bhandari <nithinibhandari2017@gmail.com>
2025-04-21 10:29:32 +0530

    update site URL in Docusaurus configuration to reflect the new domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/logoAiNotesXyz.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ai-notes.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
