5546aea96cf64668d4da4bf6ac4cb290bf5bf06a
Nithin I Bhandari <nithinibhandari2017@gmail.com>
2025-10-20 11:13:58 +0530

    fix: update Clarity tracking script ID in Docusaurus configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
           c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
           t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
           y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "x75s5rlnp6");
+        })(window, document, "clarity", "script", "tsz7eagnbl");
       `,
     },
   ],
